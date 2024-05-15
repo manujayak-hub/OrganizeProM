b5c4aedf5b837016834bcc9b3ffc5faa0b09b0ef
Manujaya Kamburugamuwa <manujayak8@gmail.com>
2024-05-15 11:23:46 +0530

    new changes

--- app/src/main/java/com/example/taskapp/ScreenOne.kt
+++ app/src/main/java/com/example/taskapp/ScreenOne.kt
@@ -15,7 +15,7 @@ class ScreenOne : AppCompatActivity() {
 
         logoLoad.alpha = 0f
         logoLoad.animate().setDuration(5000).alpha(1f).withEndAction {
-            val i = Intent(this, ScreenTwo::class.java) // Replace NextActivity with the actual name of your next activity
+            val i = Intent(this, MainActivity::class.java) // Replace NextActivity with the actual name of your next activity
             startActivity(i)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
